c4e759a1cc68bb4dea6940ae0a521dc350d52c2e
Dima <myuziw8s1t@gmail.com>
2020-10-28 20:45:23 -0400

    Test server on WSL1

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Col, Row } from 'react-bootstrap';
-import products from '../products';
+import axios from 'axios';
 import Product from '../components/Product';
 
 const HomeScreen = () => {
+  const [product, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await axios.get('/');
+      console.log(res.data);
+      // setProducts(data);
+    };
+    // console.log('products:', products);
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <h1>Latest Products</h1>
       <Row>
-        {products.map((product) => (
+        {/* {console.log('CLG from react: ', products)} */}
+        {/* {products.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
             <Product product={product} />
           </Col>
-        ))}
+        ))} */}
       </Row>
     </>
   );
