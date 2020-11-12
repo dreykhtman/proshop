930a21ac81c31585e93723f989be7185153791f0
Dima <myuziw8s1t@gmail.com>
2020-11-12 18:21:34 -0500

    7.42 Add user registration route and password encription

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -27,9 +27,19 @@ const userSchema = mongoose.Schema(
   }
 );
 
+/* eslint-disable func-names */
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return bcrypt.compare(enteredPassword, this.password);
 };
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model('User', userSchema);
 
