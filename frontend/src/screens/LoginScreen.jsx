8b47e5c88f4c8a12f5e46bc429dca4a304b86791
Dima <myuziw8s1t@gmail.com>
2020-11-14 17:00:48 -0500

    8.45 Add log out functionality to Header

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -13,8 +13,7 @@ const LoginScreen = ({ location, history }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const userLogin = useSelector((state) => state.userLogin);
-  const { loading, error, userInfo } = userLogin;
+  const { loading, error, userInfo } = useSelector((state) => state.userLogin);
 
   const redirect = location.search ? location.search.split('=')[1] : '/';
 
