e35d677e661a4de784c5abe94d200c5a8cd10a2c
Dima <myuziw8s1t@gmail.com>
2020-11-20 20:20:12 -0500

    9.54 Build PlaceOrder screen

---
+++ frontend/src/screens/PlaceOrderScreen.jsx
@@ -0,0 +1,126 @@
+import React, { useState } from 'react';
+import { Row, Col, ListGroup, Image, Card, Button } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { saveShippingAddress } from '../actions/cartActions';
+
+import Message from '../components/Message';
+import CheckoutSteps from './CheckoutSteps';
+
+const PlaceOrderScreen = () => {
+  const cart = useSelector((state) => state.cart);
+
+  cart.itemsPrice = cart.cartItems.reduce((acc, item) => {
+    return acc + item.price * item.qty;
+  }, 0);
+  cart.shippingPrice = cart.itemsPrice > 100 ? 0 : 8;
+  cart.taxPrice = cart.itemsPrice * 0.15;
+  cart.totalPrice =
+    Math.round((cart.itemsPrice + cart.shippingPrice + cart.taxPrice) * 100) /
+    100;
+
+  const placeOrderHandler = () => {};
+
+  return (
+    <>
+      <CheckoutSteps step1 step2 step3 step4 />
+      <Row>
+        <Col md={8}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h4>Shipping Address</h4>
+              <p>
+                {cart.shippingAddress.address}, {cart.shippingAddress.city}{' '}
+                {cart.shippingAddress.postalCode},{' '}
+                {cart.shippingAddress.country}
+              </p>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <h4>Payment Method</h4>
+              {cart.paymentMethod}
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <h4>Order Items</h4>
+              {cart.cartItems.length === 0 ? (
+                <Message>Your cart is empty</Message>
+              ) : (
+                <ListGroup variant="flush">
+                  {cart.cartItems.map((item, index) => (
+                    <ListGroup.Item key={index}>
+                      <Row>
+                        <Col md={1}>
+                          <Image
+                            src={item.image}
+                            alt={item.name}
+                            fluid
+                            rounded
+                          />
+                        </Col>
+                        <Col>
+                          <Link to={`/product/${item.product}`}>
+                            {item.name}
+                          </Link>
+                        </Col>
+                        <Col md={4}>
+                          {item.qty} x ${item.price} = ${item.qty * item.price}
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              )}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+
+        <Col md={4}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <h4>Order Summary</h4>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col className="font-weight-bold">Items</Col>
+                  <Col>${cart.itemsPrice.toFixed(2)}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col className="font-weight-bold">Shipping</Col>
+                  <Col>${cart.shippingPrice.toFixed(2)}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col className="font-weight-bold">Tax</Col>
+                  <Col>${cart.taxPrice.toFixed(2)}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col className="font-weight-bold">Total</Col>
+                  <Col>${cart.totalPrice.toFixed(2)}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  type="button"
+                  className="btn-block"
+                  disabled={cart.cartItems.length === 0}
+                  onClick={placeOrderHandler}
+                >
+                  Place Order
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default PlaceOrderScreen;
