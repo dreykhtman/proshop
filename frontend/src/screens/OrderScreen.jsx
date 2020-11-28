25ff8001a5e7283fcc0641f41c7b5cdeeace69f1
Dima <myuziw8s1t@gmail.com>
2020-11-27 22:13:57 -0500

    10.59 Build OrderScreen

---
+++ frontend/src/screens/OrderScreen.jsx
@@ -0,0 +1,136 @@
+import React, { useEffect } from 'react';
+import { Row, Col, ListGroup, Image, Card } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import { getOrderDetails } from '../actions/orderActions';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+
+const OrderScreen = ({ match }) => {
+  const dispatch = useDispatch();
+  const orderId = match.params.id;
+  const { order, loading, error } = useSelector((state) => state.orderDetails);
+
+  useEffect(() => {
+    dispatch(getOrderDetails(orderId));
+  }, [orderId, dispatch]);
+
+  return loading ? (
+    <Loader />
+  ) : error ? (
+    <Message variant="danger">{error}</Message>
+  ) : (
+    <>
+      <h1>Order {order._id}</h1>
+      <Row>
+        <Col md={8}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h4>Shipping Information</h4>
+              <p>
+                <strong className="font-weight-bold">Name: </strong>
+                {order.user.name}
+              </p>
+              <p>
+                <strong className="font-weight-bold">Email: </strong>
+                <a href={`mailto:${order.user.email}`}>{order.user.email}</a>
+              </p>
+              <p>
+                <strong className="font-weight-bold">Address: </strong>
+                {order.shippingAddress.address}, {order.shippingAddress.city}{' '}
+                {order.shippingAddress.postalCode},{' '}
+                {order.shippingAddress.country}
+              </p>
+              {order.isDelivered ? (
+                <Message variant="success">
+                  Delivered on {order.deliveredAt}
+                </Message>
+              ) : (
+                <Message variant="warning">Not Delivered</Message>
+              )}
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <h4>Payment Method</h4>
+              <p>{order.paymentMethod}</p>
+              {order.isPaid ? (
+                <Message variant="success">Paid on {order.paidAt}</Message>
+              ) : (
+                <Message variant="warning">Not Paid</Message>
+              )}
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <h4>Order Items</h4>
+              {order.orderItems.length === 0 ? (
+                <Message>Order is empty</Message>
+              ) : (
+                <ListGroup variant="flush">
+                  {order.orderItems.map((item, index) => (
+                    <ListGroup.Item key={index}>
+                      <Row>
+                        <Col md={1}>
+                          <Image
+                            src={item.image}
+                            alt={item.name}
+                            fluid
+                            rounded
+                          />
+                        </Col>
+                        <Col>
+                          <Link to={`/product/${item.product}`}>
+                            {item.name}
+                          </Link>
+                        </Col>
+                        <Col md={4}>
+                          {item.qty} x ${item.price} = ${item.qty * item.price}
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              )}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+
+        <Col md={4}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <h4>Order Summary</h4>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col className="font-weight-bold">Items</Col>
+                  <Col>${order.itemsPrice.toFixed(2)}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col className="font-weight-bold">Shipping</Col>
+                  <Col>${order.shippingPrice.toFixed(2)}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col className="font-weight-bold">Tax</Col>
+                  <Col>${order.taxPrice.toFixed(2)}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col className="font-weight-bold">Total</Col>
+                  <Col>${order.totalPrice.toFixed(2)}</Col>
+                </Row>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default OrderScreen;
