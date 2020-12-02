73a457dc630eef31a14d23402f3bf269ffa8b46d
Dima <myuziw8s1t@gmail.com>
2020-12-01 21:18:00 -0500

    10.65 User details and order reset

--- frontend/src/actions/userActons.js
+++ frontend/src/actions/userActons.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+import { ORDER_LIST_MY_RESET } from '../constants/orderConstants';
 import {
   USER_DETAILS_FAIL,
   USER_DETAILS_REQUEST,
+  USER_DETAILS_RESET,
   USER_DETAILS_SUCCESS,
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
@@ -84,6 +86,8 @@ export const login = (email, password) => async (dispatch) => {
 export const logout = () => (dispatch) => {
   localStorage.removeItem('userInfo');
   dispatch({ type: USER_LOGOUT });
+  dispatch({ type: USER_DETAILS_RESET });
+  dispatch({ type: ORDER_LIST_MY_RESET });
 };
 
 export const getUserDetails = (id) => async (dispatch, getState) => {
