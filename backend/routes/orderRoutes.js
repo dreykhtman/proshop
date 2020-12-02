1f1dbf5b595a10d84339ddc5bc8d0cd3525c2d0f
Dima <myuziw8s1t@gmail.com>
2020-12-01 21:08:17 -0500

    10.64 Add functionality to list all orders on user's profile

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,6 +3,7 @@ import express from 'express';
 
 import {
   addOrderItems,
+  getMyOrders,
   getOrderById,
   updateOrderToPaid,
 } from '../controllers/orderController.js';
@@ -11,6 +12,7 @@ import { protect } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 router.route('/').post(protect, addOrderItems);
+router.route('/my-orders').get(protect, getMyOrders);
 router.route('/:id').get(protect, getOrderById);
 router.route('/:id/pay').put(protect, updateOrderToPaid);
 
