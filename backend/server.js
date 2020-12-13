9e831265ec1581a7ba4b189eaa565fd5616707a3
Dima <myuziw8s1t@gmail.com>
2020-12-12 23:24:21 -0500

    12.79 Set up image upload

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/extensions */
 import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
 // eslint-disable-next-line no-unused-vars
 import colors from 'colors';
@@ -8,6 +9,7 @@ import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
@@ -25,10 +27,14 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
 
 app.get('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID);
 });
+
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.use(notFound);
 
