3b028d1b32fbdc7cb85cb4466cb010b8192d337c
Dima <myuziw8s1t@gmail.com>
2020-12-17 18:32:54 -0500

    13.84 Add product review functionality

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -8,6 +8,7 @@ import {
   productDeleteReducer,
   productCreateReducer,
   productUpdateReducer,
+  productReviewCreateReducer,
 } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
 import {
@@ -34,6 +35,7 @@ const reducer = combineReducers({
   productCreate: productCreateReducer,
   productUpdate: productUpdateReducer,
   productDelete: productDeleteReducer,
+  productReviewCreate: productReviewCreateReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
