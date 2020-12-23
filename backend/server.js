b63d453efb1934ef857c3fbce8a42b2c9d55ce5a
Dima <myuziw8s1t@gmail.com>
2020-12-22 22:56:09 -0500

    Replace port 5000 with a variable

--- backend/server.js
+++ backend/server.js
@@ -56,7 +56,7 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 app.listen(
-  5000,
+  PORT,
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   )
